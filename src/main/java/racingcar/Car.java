a7ea96e8e30c25e749cba011e4c8e90638484627
koomin1227 <koomin1227@naver.com>
2024-01-05 00:55:34 +0900

    feat: 자동차 객체 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,29 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int moveDistance;
+    public Car() {
+        moveDistance = 0;
+    }
+
+    public void move() {
+        int moveVariance = Randoms.pickNumberInRange(0,9);
+        if (moveVariance >= 4) {
+            moveDistance += 1;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoveDistance() {
+        return moveDistance;
+    }
+}
