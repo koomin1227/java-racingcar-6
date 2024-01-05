141b4cbb3b63121549c9a1b6863c1c3b1e7cb9f7
koomin1227 <koomin1227@naver.com>
2024-01-05 18:32:16 +0900

    feat: 우승자를 계산하여 출력하는 기능 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -8,6 +8,7 @@ public class RacingCar {
     public void run() {
         setGame();
         playRace();
+        outputWinner();
     }
     private void setGame() {
         Output.printNamesInputMessage();
@@ -27,6 +28,24 @@ public class RacingCar {
             }
             Output.printResultByDegree(cars);
         }
+
+    }
+
+    private void outputWinner() {
+        int maxDistance = 0;
+        for (Car car : cars) {
+            int distance = car.getMoveDistance();
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+        ArrayList<Car> winnerCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getMoveDistance() == maxDistance) {
+                winnerCars.add(car);
+            }
+        }
+        Output.printWinner(winnerCars);
     }
 
     public ArrayList<Car> getCars() {
