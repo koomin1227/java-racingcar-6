02903b1f0029d7322c60a5682038c09ee8ff4fed
koomin1227 <koomin1227@naver.com>
2024-01-05 18:14:28 +0900

    feat: 회차별 결과 출력 기능 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Output {
     public static void printNamesInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +9,22 @@ public class Output {
 
     public static void printCountInputMessage() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printResultByDegree(ArrayList<Car> cars) {
+        System.out.println("\n실행 결과");
+        for (Car car: cars) {
+            System.out.printf("%s : ", car.getName());
+            printDistance(car);
+        }
+    }
+
+    private static void printDistance(Car car) {
+        int distance = car.getMoveDistance();
+        StringBuilder distanceResult = new StringBuilder();
+        for (int i = 0; i < distance; i++) {
+            distanceResult.append('-');
+        }
+        System.out.println(distanceResult);
     }
 }
